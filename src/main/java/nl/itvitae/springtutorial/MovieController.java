3625c108f969fa8854ff2f66eb4a95ecf67427d6
Eric-Wubbo Lameijer <eric_wubbo@hotmail.com>
2024-02-25 09:32:58 +0100

    006: using ids instead of making a separate path for every movie

--- src/main/java/nl/itvitae/springtutorial/MovieController.java
+++ src/main/java/nl/itvitae/springtutorial/MovieController.java
@@ -1,20 +1,23 @@
 package nl.itvitae.springtutorial;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class MovieController {
 
     record Movie(String title, int rating) {}
 
-    @GetMapping("up")
-    public Movie getUp() {
-        return new Movie("Up", 5);
-    }
+    private List<Movie> movies = List.of(
+            new Movie("Up", 5),
+            new Movie("Citizen Kane", 2),
+            new Movie("The Grand Budapest Hotel", 3));
 
-    @GetMapping("citizen-kane")
-    public Movie getCitizenKane() {
-        return new Movie("Citizen Kane", 2);
+    @GetMapping("{id}")
+    public Movie getMovieById(@PathVariable int id) {
+        return movies.get(id);
     }
 }
