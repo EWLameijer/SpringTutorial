5b2c993cb89a431c220df2018660f5ebf7c6879f
Eric-Wubbo Lameijer <eric_wubbo@hotmail.com>
2024-02-25 07:42:36 +0100

    Showing the controller

---
+++ src/main/java/nl/itvitae/springtutorial/Controller.java
@@ -0,0 +1,12 @@
+package nl.itvitae.springtutorial;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping
+    public String message() {
+        return "Welcome to this beautiful REST controller!";
+    }
+}
