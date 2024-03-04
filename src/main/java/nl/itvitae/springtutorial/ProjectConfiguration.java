eea59f51dd891bd4aa0d8503cf42059856c5398c
EWLameijer <eric_wubbo@hotmail.com>
2024-03-04 11:23:29 +0100

    054: allow POST by disabling CSRF

--- src/main/java/nl/itvitae/springtutorial/ProjectConfiguration.java
+++ src/main/java/nl/itvitae/springtutorial/ProjectConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
 import javax.sql.DataSource;
 
@@ -29,6 +30,7 @@ public class ProjectConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .httpBasic(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(requests ->
                         requests
                                 .requestMatchers("/users/register").permitAll()
