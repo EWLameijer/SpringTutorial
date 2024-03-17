bf8ba500def750eaf49fa9268413d04d13f3ac92
Eric-Wubbo Lameijer <eric_wubbo@hotmail.com>
2024-03-17 20:42:49 +0100

    removed extraneous username from ReviewInputDto

--- src/main/java/nl/itvitae/springtutorial/review/ReviewInputDto.java
+++ src/main/java/nl/itvitae/springtutorial/review/ReviewInputDto.java
@@ -1,4 +1,4 @@
 package nl.itvitae.springtutorial.review;
 
-public record ReviewInputDto(String username, Long movieId, int rating, String text) {
+public record ReviewInputDto(Long movieId, int rating, String text) {
 }
