f5c51ee768947db054e599889eb1bcc19835fe89
Eric-Wubbo Lameijer <eric_wubbo@hotmail.com>
2024-03-23 19:00:57 +0100

    061: requestMatchers with HTTP verbs

--- src/main/java/nl/itvitae/springtutorial/ProjectConfiguration.java
+++ src/main/java/nl/itvitae/springtutorial/ProjectConfiguration.java
@@ -2,14 +2,15 @@ package nl.itvitae.springtutorial;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
 import javax.sql.DataSource;
 
@@ -34,7 +35,7 @@ public class ProjectConfiguration {
                 .authorizeHttpRequests(requests ->
                         requests
                                 .requestMatchers("/users/register").permitAll()
-                                .requestMatchers("/movies/*").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/movies/**").permitAll()
                                 .anyRequest().authenticated()
                 )
                 .build();
